a69128b04b2f0fdbeefc7b95d0c047d5a6ef7712
Ryan Jones <ryan.jvjones@ntlworld.com>
2022-07-04 17:15:21 +0100

    Create VendingServiceLayerImplTest.java

---
+++ vending-machine/src/test/java/Service/VendingServiceLayerImplTest.java
@@ -0,0 +1,12 @@
+package Service;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VendingServiceLayerImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
